efe82129079812a92f0bfd695105b14deb1f295b
Ruud Renssen <ruud@vicompany.nl>
2020-03-05 11:55:49 +0100

    replace function with async await

--- public/js/main.js
+++ public/js/main.js
@@ -8,123 +8,134 @@ import {
 	contributorsTpl,
 } from './templates.js';
 
-const API_URL = 'https://api.github.com';
+const API_URL = 'https://api.github.com/orgs/vicompany';
 const USE_CACHE = true;
 
-// TODO: refactor into something more elegant.
-// Luke, use the fetch!
-const getJSON = (url, callback) => {
-	const isError = Math.floor(Math.random() * 10) === 0; // 10% error chance
-
-	if (isError) {
-		return callback(new Error('Network error!'));
-	}
-
+async function getJSON(url) {
 	const fromCache = USE_CACHE && cache.get(url);
 
 	if (fromCache) {
-		return callback(null, fromCache);
+		return fromCache;
 	}
 
-	fetch(url)
-		.then((res) => {
-			res.json()
-				.then((json) => {
-					if (USE_CACHE) {
-						cache.set(url, json);
-					}
+	try {
+		const response = await fetch(url); // fetch the url and await its response
+		const data = await response.json(); // wait for the response and turn it into json
 
-					callback(null, json);
-				});
-		});
-};
-
-const getRepos = (url, callback) => {
-	getJSON(url, (err, repos = []) => {
-		if (err) {
-			return callback(new Error('They took my repos. Dook err derr!'));
+		if (USE_CACHE) {
+			cache.set(url, data);
 		}
 
-		repos = repos
-			.filter(r => r.fork === false) // No forks, no forks!
-			.sort(r => new Date(r.updated_at).getTime());
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+}
 
-		callback(null, repos);
-	});
-};
+async function init() {
+	const companyData = await getJSON(API_URL);
+	let repositoryData = await getJSON(`${companyData.repos_url}`);
 
-// IIFE to kick it all off
-(() => {
-	getJSON(`${API_URL}/orgs/vicompany`, (err, org) => {
-		const el = document.querySelector('#org');
+	repositoryData = repositoryData
+		.filter(r => r.fork === false) // No forks, no forks!
+		.sort(r => new Date(r.updated_at).getTime());
 
-		if (err) {
-			return renderError(err);
-		}
+	const companyEl = document.querySelector('#org');
+	const reposEl = document.querySelector('#repos');
 
-		render(el, org, orgTpl);
+	render(companyEl, companyData, orgTpl);
+	render(reposEl, repositoryData, reposTpl);
+}
 
-		const { repos_url: reposUrl } = org;
+init();
 
-		getRepos(reposUrl, (err, repos) => {
-			const reposEl = document.querySelector('#repos');
 
-			if (err) {
-				return renderError(err);
-			}
+// const getRepos = (url, callback) => {
+// 	getJSON(url, (err, repos = []) => {
+// 		if (err) {
+// 			return callback(new Error('They took my repos. Dook err derr!'));
+// 		}
 
-			render(reposEl, repos, reposTpl);
-		});
-	});
+// 		repos = repos
+// 			.filter(r => r.fork === false) // No forks, no forks!
+// 			.sort(r => new Date(r.updated_at).getTime());
 
-	document
-		.querySelector('main')
-		.addEventListener('click', (e) => {
-			const { target } = e;
-			const modal = document.querySelector('#modal');
+// 		callback(null, repos);
+// 	});
+// };
 
-			if (target.classList.contains('js-repo')) {
-				e.preventDefault();
+// // IIFE to kick it all off
+// (() => {
+// 	getJSON(`${API_URL}/orgs/vicompany`, (err, org) => {
+// 		const el = document.querySelector('#org');
 
-				getJSON(target.href, (err, repo) => {
-					if (err) {
-						return renderError(err);
-					}
+// 		if (err) {
+// 			return renderError(err);
+// 		}
 
-					render(modal, repo, repoTpl);
+// 		render(el, org, orgTpl);
 
-					modal.querySelector('dialog').showModal();
-				});
-			}
+// 		const { repos_url: reposUrl } = org;
 
-			if (target.classList.contains('js-contributors')) {
-				e.preventDefault();
+// 		getRepos(reposUrl, (err, repos) => {
+// 			const reposEl = document.querySelector('#repos');
 
-				getJSON(target.href, (err, contributors = []) => {
-					if (err) {
-						return renderError(err);
-					}
+// 			if (err) {
+// 				return renderError(err);
+// 			}
 
-					// TODO: get user data from all contributers e.g. https://api.github.com/users/svensigmond
-					// and replace the 'users' array with this real data.
-					const data = {
-						contributors,
-						users: contributors.map(c => ({
-							url: c.url,
-							avatar: c.avatar_url,
-							login: c.login,
-						})),
-					};
+// 			render(reposEl, repos, reposTpl);
+// 		});
+// 	});
 
-					render(modal, data, contributorsTpl);
+// 	document
+// 		.querySelector('main')
+// 		.addEventListener('click', (e) => {
+// 			const { target } = e;
+// 			const modal = document.querySelector('#modal');
 
-					modal.querySelector('dialog').showModal();
-				});
-			}
+// 			if (target.classList.contains('js-repo')) {
+// 				e.preventDefault();
 
-			if (target.classList.contains('js-modal-close')) {
-				e.preventDefault();
-				target.closest('dialog').close();
-			}
-		});
-})();
+// 				getJSON(target.href, (err, repo) => {
+// 					if (err) {
+// 						return renderError(err);
+// 					}
+
+// 					render(modal, repo, repoTpl);
+
+// 					modal.querySelector('dialog').showModal();
+// 				});
+// 			}
+
+// 			if (target.classList.contains('js-contributors')) {
+// 				e.preventDefault();
+
+// 				getJSON(target.href, (err, contributors = []) => {
+// 					if (err) {
+// 						return renderError(err);
+// 					}
+
+// 					// TODO: get user data from all contributers e.g. https://api.github.com/users/svensigmond
+// 					// and replace the 'users' array with this real data.
+// 					const data = {
+// 						contributors,
+// 						users: contributors.map(c => ({
+// 							url: c.url,
+// 							avatar: c.avatar_url,
+// 							login: c.login,
+// 						})),
+// 					};
+
+// 					render(modal, data, contributorsTpl);
+
+// 					modal.querySelector('dialog').showModal();
+// 				});
+// 			}
+
+// 			if (target.classList.contains('js-modal-close')) {
+// 				e.preventDefault();
+// 				target.closest('dialog').close();
+// 			}
+// 		});
+// })();
